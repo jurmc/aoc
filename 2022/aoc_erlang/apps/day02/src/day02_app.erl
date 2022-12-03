9940c5a54213d14ce281073639478ba50fdbb913
anon <anonymous@example.com>
2022-12-04 00:04:45 +0100

    Boilerplate for day02 added

---
+++ 2022/aoc_erlang/apps/day02/src/day02_app.erl
@@ -0,0 +1,18 @@
+%%%-------------------------------------------------------------------
+%% @doc day02 public API
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(day02_app).
+
+-behaviour(application).
+
+-export([start/2, stop/1]).
+
+start(_StartType, _StartArgs) ->
+    day02_sup:start_link().
+
+stop(_State) ->
+    ok.
+
+%% internal functions
