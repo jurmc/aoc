e8c0376fcf38b201d63bfe1b9f54fa427cf5eba5
anon <anonymous@example.com>
2022-12-04 21:23:56 +0100

    Day02 part2 solved

--- 2022/aoc_erlang/apps/day02/src/day02_app.erl
+++ 2022/aoc_erlang/apps/day02/src/day02_app.erl
@@ -10,6 +10,10 @@
 
 %%% Internal functions
 
+%% X: lose,
+%% Y: draw
+%% Z: win
+
 into_shape("A") -> rock;
 into_shape("X") -> rock;
 into_shape("B") -> paper;
@@ -35,6 +39,7 @@ result(scissors, paper ) -> lost.
 result_points(won) -> 6;
 result_points(draw) -> 3;
 result_points(lost) -> 0.
+
 shape_points(rock) -> 1;
 shape_points(paper) -> 2;
 shape_points(scissors) -> 3.
@@ -51,10 +56,6 @@ round_outcome_test() ->
 part1(InList) ->
     lists:sum(lists:map(fun(Item) -> round_outcome(Item) end, InList)).
 
-%%input_into_list(<<F,R/binary>>, Acc) when F == 10 -> input_into_list(R, [[]|Acc]);
-%%input_into_list(<<F,R/binary>>, [[]|T]) -> input_into_list(R, [<<F>>,[]|T]);
-%%input_into_list(<<F,R/binary>>, [H|T]) -> input_into_list(R, [<<F,H/binary>>|T]);
-%%input_into_list(<<F,R/binary>>, []) -> input_into_list(R, [<<F>>]).
 
 into_list(FileContent) ->
     Lines = string:tokens(erlang:binary_to_list(FileContent), "\n"),
@@ -73,6 +74,32 @@ test_input_test() ->
 normal_input_test() ->
     {ok, FileContent} = aoc_input_app:read_file("input_day02.txt"),
     Result = part1(into_list(FileContent)),
+    ?debugFmt("Result: ~p", [Result]).
+
+requested_game({HisCode, "Y"}) -> {HisCode, HisCode};
+requested_game({HisCode = "A", "X"}) -> {HisCode, "C"};
+requested_game({HisCode = "B", "X"}) -> {HisCode, "A"};
+requested_game({HisCode = "C", "X"}) -> {HisCode, "B"};
+requested_game({HisCode = "A", "Z"}) -> {HisCode, "B"};
+requested_game({HisCode = "B", "Z"}) -> {HisCode, "C"};
+requested_game({HisCode = "C", "Z"}) -> {HisCode, "A"}.
+
+requested_games(InList) ->
+    lists:map(fun requested_game/1, InList).
+
+requested_game_test() ->
+    ?assertEqual({"A", "A"}, requested_game({"A", "Y"})),
+    ?assertEqual({"B", "A"}, requested_game({"B", "X"})),
+    ?assertEqual({"C", "A"}, requested_game({"C", "Z"})).
+
+requested_games_test() ->
+    {ok, FileContent} = aoc_input_app:read_file("test_input_day02.txt"),
+    ?assertEqual([{"A", "A"}, {"B", "A"}, {"C", "A"}], requested_games(into_list(FileContent))),
+    ?assertEqual(lists:sum([4, 1, 7]), part1(requested_games(into_list(FileContent)))).
+
+real_input_requested_games_test() ->
+    {ok, FileContent} = aoc_input_app:read_file("input_day02.txt"),
+    Result = part1(requested_games(into_list(FileContent))),
     ?debugFmt("Result: ~p", [Result]).
 
 -endif.
