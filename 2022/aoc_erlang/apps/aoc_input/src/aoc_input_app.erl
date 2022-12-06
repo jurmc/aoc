acb7e5d5cb192ffbeb80dfe6e014c38d3a2ad0fc
Jurek Michal-AMJ018 <Michal.Jurek@motorolasolutions.com>
2022-12-06 21:14:23 +0100

    Day05 work in progress (where in my Erlang is lists:enumerate?).

--- 2022/aoc_erlang/apps/aoc_input/src/aoc_input_app.erl
+++ 2022/aoc_erlang/apps/aoc_input/src/aoc_input_app.erl
@@ -1,8 +1,10 @@
 -module(aoc_input_app).
 
--export([read_file/1, read_file_lines/1, read_file_lines_into_2_columns/1]).
+-export([read_file/1, read_file_lines/1, read_file_lines2/1, read_file_lines_into_2_columns/1, remove_empty_lines/1]).
 
+%%%
 %%% Reading files
+%%%
 
 read_file(FileName) ->
     file:read_file(os:getenv("AOC_INPUT") ++ "/" ++ FileName).
@@ -11,12 +13,52 @@ read_file_lines(FileName) ->
     {ok, FileContent} = read_file(FileName),
     string:tokens(erlang:binary_to_list(FileContent), "\n").
 
+%% Merge it with read_file_lines (above)
+read_file_lines2(FileName) ->
+    {ok, FileContent} = read_file(FileName),
+    file_content_into_list_of_lines(FileContent).
+
 read_file_lines_into_2_columns(FileName) ->
     into_2_columns(read_file_lines(FileName)).
 
+%%%
 %%% Conversions
+%%%
 
 %% TODO: Later this might be parametrized
 into_2_columns(Lines) ->
     lists:map(fun(Line) -> [I1, I2] = string:tokens(Line, " "), {I1, I2} end, Lines).
+
+remove_empty_lines(Lines) ->
+    lists:filter(fun(X) -> [] =/= X end, Lines).
+
+%%%
+%%% Internal functions
+%%%
+
+file_content_into_list_of_lines(FileContent) ->
+    %%[[], "ab", [], "cd", "ef"].
+    file_content_into_list_of_lines(FileContent, []).
+
+file_content_into_list_of_lines([], Acc) -> lists:reverse(Acc);
+file_content_into_list_of_lines([10|T], Acc) ->
+    file_content_into_list_of_lines(T, [[]|Acc]);
+file_content_into_list_of_lines(FileContent, Acc) ->
+    case string:split(FileContent, "\n") of
+        [Line, RestLines] -> file_content_into_list_of_lines(RestLines, [Line|Acc]);
+        [Line] -> file_content_into_list_of_lines([], [Line|Acc])
+    end.
+
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+
+file_content_into_list_of_lines_test() ->
+    ?assertEqual([[], "ab", [], "cd", "ef"], file_content_into_list_of_lines("\nab\n\ncd\nef\n")),
+    ?assertEqual(["ab", [], "cd", "ef", []], file_content_into_list_of_lines("ab\n\ncd\nef\n\n")).
+
+remove_empty_lines_test() ->
+    ?assertEqual(["ab", "cd", "ef"], remove_empty_lines([[], "ab", [], "cd", "ef"])),
+    ?assertEqual(["ab", "cd", "ef"], remove_empty_lines(["ab", [], "cd", "ef", []])).
+
+-endif.
 
--- 2022/aoc_erlang/apps/day05/src/day05_app.erl
+++ 2022/aoc_erlang/apps/day05/src/day05_app.erl
@@ -20,14 +20,51 @@
 %% move 2 from 2 to 1
 %% move 1 from 1 to 2
 %%
+
 extract_stack_line(Line) ->
-    %% TODO: fake implementation
-    "ZMP".
+    extract_stack_line([32|Line], []).
+extract_stack_line([], Acc) -> lists:reverse(Acc);
+extract_stack_line(Line, Acc) ->
+    {Column, Rest} = lists:split(4, Line),
+    StrippedColumn = lists:foldl(fun(C, LocAcc) ->
+                                         case C of
+                                             NotIgnored when NotIgnored >= 65, NotIgnored =< 90 ->[NotIgnored|LocAcc];
+                                             _ -> LocAcc
+                                         end
+                                end, [], Column),
+    extract_stack_line(Rest, [StrippedColumn|Acc]).
 
 extract_stack_line_test() ->
-    ?assertEqual("ZMP", extract_stack_line(" [Z] [M] [P]")).
+    ?assertEqual(["Z", "M", "P"], extract_stack_line("[Z] [M] [P]")),
+    ?assertEqual(["N", "C", ""],  extract_stack_line("[N] [C]    ")),
+    ?assertEqual(["", "D", ""],   extract_stack_line("    [D]    ")).
 
-read_input(_FileName) ->
+find_empty_line_idx(Lines) -> find_empty_line_idx(Lines, 0).
+find_empty_line_idx([Line|RestLines], Acc) ->
+    case string:is_empty(string:trim(Line)) of
+        true -> Acc;
+        false -> find_empty_line_idx(RestLines, Acc+1)
+    end.
+
+find_empty_line_test() ->
+    FileLines = aoc_input_app:read_file_lines2("test_input_day05.txt"),
+    ?assertEqual(4, find_empty_line_idx(FileLines)).
+
+create_stacks(StacksLinesLists) ->
+    not_implemented.
+
+create_stacks_test() ->
+    ?assertEqual(["NZ", "DCM", "P"], create_stacks([[[],"D",[]],["N","C",[]],["Z","M","P"]])).
+
+read_input(FileName) ->
+    FileLines = aoc_input_app:read_file_lines2(FileName),
+    EmptyLineIdx = find_empty_line_idx(FileLines),
+    {StackLines, [_IgnoredEmpty|MoveLines]} = lists:split(EmptyLineIdx-1, FileLines),
+
+    StacksLinesLists = lists:map(fun(Line) -> extract_stack_line(binary:bin_to_list(Line)) end, StackLines),
+    Stacks = create_stacks(StacksLinesLists),
+    ?debugFmt("Stacks: ~p\n", [Stacks]),
+
     %% TODO: fake implementation
     [["NZ", "DCM", "P"], [[1, 2, 1], [3, 1, 3], [2, 2, 1], [1, 1, 2]]].
 
