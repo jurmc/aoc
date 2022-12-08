c506427e9390a85e91486bb19819b4dbcfa0c0e5
Michal Jurek <jurmcc@gmail.com>
2022-12-09 00:28:45 +0100

    Day06 solved

--- 2022/aoc_erlang/apps/day06/src/day06_app.erl
+++ 2022/aoc_erlang/apps/day06/src/day06_app.erl
@@ -1,18 +1,58 @@
-%%%-------------------------------------------------------------------
-%% @doc day06 public API
-%% @end
-%%%-------------------------------------------------------------------
-
 -module(day06_app).
 
--behaviour(application).
+-export([part1/1, part2/1]).
 
--export([start/2, stop/1]).
+%%% Exported functions
 
-start(_StartType, _StartArgs) ->
-    day06_sup:start_link().
+part1(FileName) ->
+    {ok, FileContent} = aoc_input_app:read_file(FileName),
+    String = binary:bin_to_list(FileContent),
+    detect_marker(String, 4).
 
-stop(_State) ->
-    ok.
+part2(FileName) ->
+    {ok, FileContent} = aoc_input_app:read_file(FileName),
+    String = binary:bin_to_list(FileContent),
+    detect_marker(String, 14).
 
-%% internal functions
+%%% Internal functions
+
+detect_marker(String, Length) -> detect_marker(String, Length, Length).
+
+detect_marker(String, Length, Acc) ->
+    {AnalyzedString, Rest} = lists:split(Length, String),
+    case sets:size(sets:from_list(AnalyzedString)) of
+        Length -> {AnalyzedString, Acc};
+        _ -> detect_marker(tl(AnalyzedString) ++ Rest, Length, Acc+1)
+    end;
+detect_marker(_, _, _) -> not_implemented.
+
+
+%%% Unit tests
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+
+detect_marker_test() ->
+    Part1MarkerLen = 4,
+    ?assertEqual({"jpqm", 7} , detect_marker("mjqjpqmgbljsphdztnvjfqwrcgsmlb", Part1MarkerLen)),
+    ?assertEqual({"vwbj", 5}, detect_marker("bvwbjplbgvbhsrlpgdmjqwftvncz", Part1MarkerLen)),
+    ?assertEqual({"pdvj", 6}, detect_marker("nppdvjthqldpwncqszvftbrmjlhg", Part1MarkerLen)),
+    ?assertEqual({"rfnt", 10}, detect_marker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", Part1MarkerLen)),
+    ?assertEqual({"zqfr", 11}, detect_marker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", Part1MarkerLen)),
+
+    Part2MarkerLen = 14,
+    ?assertEqual(19, element(2, detect_marker("mjqjpqmgbljsphdztnvjfqwrcgsmlb", Part2MarkerLen))),
+    ?assertEqual(23, element(2, detect_marker("bvwbjplbgvbhsrlpgdmjqwftvncz", Part2MarkerLen))),
+    ?assertEqual(23, element(2, detect_marker("nppdvjthqldpwncqszvftbrmjlhg", Part2MarkerLen))),
+    ?assertEqual(29, element(2, detect_marker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", Part2MarkerLen))),
+    ?assertEqual(26, element(2, detect_marker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", Part2MarkerLen))).
+
+
+part1_test() ->
+    {Marker, Position} = part1("test_input_day06.txt"),
+    ?debugFmt("Part1, Marker: ~p, Position: ~p\n", [Marker, Position]).
+
+part2_test() ->
+    {Marker, Position} = part2("test_input_day06.txt"),
+    ?debugFmt("Part2, Marker: ~p, Position: ~p\n", [Marker, Position]).
+
+-endif.
