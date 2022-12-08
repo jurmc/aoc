61e7dcf5a1f670e69db538ae0377fef32c1eece7
Jurek Michal-AMJ018 <Michal.Jurek@motorolasolutions.com>
2022-12-08 16:51:18 +0100

    Code structure cleaned up

--- 2022/aoc_erlang/apps/day05/src/day05_app.erl
+++ 2022/aoc_erlang/apps/day05/src/day05_app.erl
@@ -1,14 +1,58 @@
 -module(day05_app).
 
-%%-export([part1/1, part2/1]).
+-export([part1/1, part2/1]).
 
 %%% Exported functions
 
+part1(FileName) ->
+    [Stacks, Moves] = read_input(FileName),
+    FinalStacks = apply_moves(Stacks, Moves),
+    lists:map(fun([H|_]) -> H end, FinalStacks).
+
+part2(FileName) ->
+    [Stacks, Moves] = read_input(FileName),
+    FinalStacks = apply_moves(Stacks, Moves),
+    lists:map(fun([H|_]) -> H end, FinalStacks).
+
 %%% Internal functions
 
-%%% Unit tests
--ifdef(TEST).
--include_lib("eunit/include/eunit.hrl").
+apply_move(Stacks, [What, From, Where]) ->
+    Dict1 = orddict:from_list(lists:enumerate(Stacks)),
+
+    {InStack, _} = orddict:take(From, Dict1),
+    {OutStack, _} = orddict:take(Where, Dict1),
+
+    %% Update 'From' stack
+    {MovingPart, InStackUpdated} = lists:split(What, InStack),
+    Dict2 = orddict:store(From, InStackUpdated, Dict1),
+
+    %% TODO: Reverse what we move (this part is different for part1 and part2)
+    MovingPartReversed = lists:reverse(MovingPart),
+    %% TODO: it is only quick hack for part two
+    %%MovingPartReversed = MovingPart,
+
+    %% Update 'Where/To' stack
+    OutStackUpdated = MovingPartReversed ++ OutStack,
+    Dict3 = orddict:store(Where, OutStackUpdated, Dict2),
+
+    lists:map(fun({_Key, Val}) -> Val end, orddict:to_list(Dict3)).
+
+apply_moves(Stacks, Moves) ->
+    lists:foldl(fun(Move, Acc) -> apply_move(Acc, Move) end, Stacks, Moves).
+
+find_empty_line_idx(Lines) -> find_empty_line_idx(Lines, 0).
+find_empty_line_idx([Line|RestLines], Acc) ->
+    case string:is_empty(string:trim(Line)) of
+        true -> Acc;
+        false -> find_empty_line_idx(RestLines, Acc+1)
+    end.
+
+create_stacks(StacksLinesLists) ->
+    TransposedLists = transpose(StacksLinesLists),
+    lists:map(fun(List) ->
+                      lists:flatten(lists:filter(fun(C) -> C =/= [] end, List))
+              end,
+              TransposedLists).
 
 extract_stack_line(Line) ->
     extract_stack_line([32|Line], []).
@@ -23,35 +67,6 @@ extract_stack_line(Line, Acc) ->
                                 end, [], Column),
     extract_stack_line(Rest, [StrippedColumn|Acc]).
 
-extract_stack_line_test() ->
-    ?assertEqual(["Z", "M", "P"], extract_stack_line("[Z] [M] [P]")),
-    ?assertEqual(["N", "C", ""],  extract_stack_line("[N] [C]    ")),
-    ?assertEqual(["", "D", ""],   extract_stack_line("    [D]    ")).
-
-extract_move_line(Line) ->
-    [_, What, _, From, _, Where] = string:tokens(Line, " "),
-    lists:map(fun(Str) ->  {Int, _} = string:to_integer(Str), Int end, [What, From, Where]).
-
-extract_move_line_test() ->
-    ?assertEqual([1, 2, 1], extract_move_line("move 1 from 2 to 1")).
-
-
-find_empty_line_idx(Lines) -> find_empty_line_idx(Lines, 0).
-find_empty_line_idx([Line|RestLines], Acc) ->
-    case string:is_empty(string:trim(Line)) of
-        true -> Acc;
-        false -> find_empty_line_idx(RestLines, Acc+1)
-    end.
-
-find_empty_line_test() ->
-    FileLines = aoc_input_app:read_file_lines2("test_input_day05.txt"),
-    ?assertEqual(4, find_empty_line_idx(FileLines)).
-
-slice(ToSlice) ->
-    [SliceNotReversed, RestNotReversed] = lists:foldl(fun([H|T], [Col, Rest]) -> [[H|Col], [T|Rest]] end, [[],[]], ToSlice),
-    Out = [lists:reverse(SliceNotReversed), lists:reverse(RestNotReversed)],
-    Out.
-
 transpose(Lists) -> transpose(Lists, []).
 transpose(Lists, Acc) ->
     [Col,RestLists] = slice(Lists),
@@ -62,15 +77,14 @@ transpose(Lists, Acc) ->
         _ -> transpose(RestLists, [Col|Acc])
     end.
 
-create_stacks(StacksLinesLists) ->
-    TransposedLists = transpose(StacksLinesLists),
-    lists:map(fun(List) ->
-                      lists:flatten(lists:filter(fun(C) -> C =/= [] end, List))
-              end,
-              TransposedLists).
+slice(ToSlice) ->
+    [SliceNotReversed, RestNotReversed] = lists:foldl(fun([H|T], [Col, Rest]) -> [[H|Col], [T|Rest]] end, [[],[]], ToSlice),
+    Out = [lists:reverse(SliceNotReversed), lists:reverse(RestNotReversed)],
+    Out.
 
-create_stacks_test() ->
-    ?assertEqual(["NZ", "DCM", "P"], create_stacks([[[],"D",[]],["N","C",[]],["Z","M","P"]])).
+extract_move_line(Line) ->
+    [_, What, _, From, _, Where] = string:tokens(Line, " "),
+    lists:map(fun(Str) ->  {Int, _} = string:to_integer(Str), Int end, [What, From, Where]).
 
 read_input(FileName) ->
     FileLines = aoc_input_app:read_file_lines2(FileName),
@@ -89,6 +103,27 @@ read_input(FileName) ->
 
     %% TODO: partially fake implementation
     [Stacks, Moves].
+
+
+%%% Unit tests
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+
+extract_stack_line_test() ->
+    ?assertEqual(["Z", "M", "P"], extract_stack_line("[Z] [M] [P]")),
+    ?assertEqual(["N", "C", ""],  extract_stack_line("[N] [C]    ")),
+    ?assertEqual(["", "D", ""],   extract_stack_line("    [D]    ")).
+
+extract_move_line_test() ->
+    ?assertEqual([1, 2, 1], extract_move_line("move 1 from 2 to 1")).
+
+
+find_empty_line_test() ->
+    FileLines = aoc_input_app:read_file_lines2("test_input_day05.txt"),
+    ?assertEqual(4, find_empty_line_idx(FileLines)).
+
+create_stacks_test() ->
+    ?assertEqual(["NZ", "DCM", "P"], create_stacks([[[],"D",[]],["N","C",[]],["Z","M","P"]])).
 
 read_input_test() ->
     [Stacks, Moves] = read_input("test_input_day05.txt"),
@@ -115,46 +150,12 @@ read_input_test() ->
 %%
 
 %% TODO: Where -> To
-apply_move(Stacks, [What, From, Where]) ->
-    Dict1 = orddict:from_list(lists:enumerate(Stacks)),
-
-    {InStack, _} = orddict:take(From, Dict1),
-    {OutStack, _} = orddict:take(Where, Dict1),
-
-    %% Update 'From' stack
-    {MovingPart, InStackUpdated} = lists:split(What, InStack),
-    Dict2 = orddict:store(From, InStackUpdated, Dict1),
-
-    %% TODO: Reverse what we move (this part is different for part1 and part2)
-    MovingPartReversed = lists:reverse(MovingPart),
-    %% TODO: it is only quick hack for part two
-    %%MovingPartReversed = MovingPart,
-
-    %% Update 'Where/To' stack
-    OutStackUpdated = MovingPartReversed ++ OutStack,
-    Dict3 = orddict:store(Where, OutStackUpdated, Dict2),
-
-    lists:map(fun({_Key, Val}) -> Val end, orddict:to_list(Dict3)).
-
 apply_move_test() ->
     ?assertEqual([ "DNZ", "CM", "P" ], apply_move([ "NZ", "DCM", "P" ], [1, 2, 1])).
-
-apply_moves(Stacks, Moves) ->
-    lists:foldl(fun(Move, Acc) -> apply_move(Acc, Move) end, Stacks, Moves).
 
 apply_moves_test() ->
     [Stacks, Moves] = read_input("test_input_day05.txt"),
     ?assertEqual(["C", "M", "ZNDP"], apply_moves(Stacks, Moves)).
-
-part1(FileName) ->
-    [Stacks, Moves] = read_input(FileName),
-    FinalStacks = apply_moves(Stacks, Moves),
-    lists:map(fun([H|_]) -> H end, FinalStacks).
-
-part2(FileName) ->
-    [Stacks, Moves] = read_input(FileName),
-    FinalStacks = apply_moves(Stacks, Moves),
-    lists:map(fun([H|_]) -> H end, FinalStacks).
 
 part1_test() ->
     TestFileName = "test_input_day05.txt",
