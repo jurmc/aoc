8b7de61d843b0bc9350a9b788bff73df239d624c
Jurek Michal-AMJ018 <Michal.Jurek@motorolasolutions.com>
2022-12-09 17:12:42 +0100

    Initial work on day07

--- 2022/aoc_erlang/apps/day07/src/day07_app.erl
+++ 2022/aoc_erlang/apps/day07/src/day07_app.erl
@@ -1,18 +1,48 @@
-%%%-------------------------------------------------------------------
-%% @doc day07 public API
-%% @end
-%%%-------------------------------------------------------------------
-
 -module(day07_app).
 
--behaviour(application).
+-export([part1/1, part2/1]).
 
--export([start/2, stop/1]).
+%%% Exported functions
 
-start(_StartType, _StartArgs) ->
-    day07_sup:start_link().
-
-stop(_State) ->
+part1(_FileName) ->
     ok.
 
-%% internal functions
+part2(_FileName) ->
+    ok.
+
+%%% Internal functions
+
+%%% Unit tests
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+
+%% $ ls
+%% dir a
+%% 14848514 b.txt
+%% 8504156 c.dat
+%% dir d
+%% $ cd a
+
+process_dir_listing(DirListing) ->
+    [Dirs, Files] = lists:foldl(fun(Item, [Dirs, Files]) ->
+                        case Item of
+                            ["dir", DirName] -> [[DirName|Dirs], Files];
+                            [FileSize, FileName] -> [Dirs, [{FileName, element(1, string:to_integer(FileSize))}|Files]];
+                            NotImplemented -> ?debugFmt("NotImplemented: ~p\n", [NotImplemented])
+                        end
+                end,
+                [[], []],
+                DirListing),
+    [lists:reverse(Dirs), lists:reverse(Files)].
+
+process_dir_listing_test() ->
+    DirListing = [["dir", "a"], ["14848514", "b.txt"], ["8504156", "c.dat"], ["dir", "d"]],
+    ExpectedDirContent = [["a", "d"], [
+                             {"b.txt", 14848514},
+                             {"c.dat", 8504156}
+                                      ]
+                         ],
+
+    ?assertEqual(ExpectedDirContent, process_dir_listing(DirListing)).
+
+-endif.
