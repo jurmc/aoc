b0e01b9689a64cf8952cda269638dc388cf0d8e9
Michal Jurek <jurmcc@gmail.com>
2022-12-10 22:29:16 +0100

    Day07 solved.

--- 2022/aoc_erlang/apps/aoc_input/src/aoc_input_app.erl
+++ 2022/aoc_erlang/apps/aoc_input/src/aoc_input_app.erl
@@ -1,6 +1,6 @@
 -module(aoc_input_app).
 
--export([read_file/1, read_file_lines/1, read_file_lines2/1, read_file_lines_into_2_columns/1, remove_empty_lines/1]).
+-export([read_file/1, read_file_lines/1, read_file_lines/2, read_file_lines2/1, read_file_lines_into_2_columns/1, remove_empty_lines/1]).
 
 %%%
 %%% Reading files
@@ -9,15 +9,28 @@
 read_file(FileName) ->
     file:read_file(os:getenv("AOC_INPUT") ++ "/" ++ FileName).
 
+
 read_file_lines(FileName) ->
     {ok, FileContent} = read_file(FileName),
     string:tokens(erlang:binary_to_list(FileContent), "\n").
 
-%% Merge it with read_file_lines (above)
+% TODO: for now only legal option is: split_lines_into_words
+read_file_lines(FileName, [Opts]) ->
+    InLines = read_file_lines(FileName),
+
+    OutLines = case Opts of
+                   split_lines_into_words ->
+                       lists:map(fun(Line) -> string:tokens(Line, " ") end, InLines);
+                   _ -> InLines
+               end,
+    OutLines.
+
+%% TODO: Merge it with read_file_lines (above)
 read_file_lines2(FileName) ->
     {ok, FileContent} = read_file(FileName),
     file_content_into_list_of_lines(FileContent).
 
+%% TODO: Somethin more generic?
 read_file_lines_into_2_columns(FileName) ->
     into_2_columns(read_file_lines(FileName)).
 
@@ -26,6 +39,7 @@ read_file_lines_into_2_columns(FileName) ->
 %%%
 
 %% TODO: Later this might be parametrized
+%% TODO: or sucha converstions will become internal and read_file_lines/2 have to be used with proper Opts
 into_2_columns(Lines) ->
     lists:map(fun(Line) -> [I1, I2] = string:tokens(Line, " "), {I1, I2} end, Lines).
 
--- 2022/aoc_erlang/apps/day07/src/day07_app.erl
+++ 2022/aoc_erlang/apps/day07/src/day07_app.erl
@@ -1,14 +1,8 @@
 -module(day07_app).
 
--export([part1/1, part2/1]).
+%%-export([part1/1, part2/1]).
 
 %%% Exported functions
-
-part1(_FileName) ->
-    ok.
-
-part2(_FileName) ->
-    ok.
 
 %%% Internal functions
 
@@ -44,5 +38,192 @@ process_dir_listing_test() ->
                          ],
 
     ?assertEqual(ExpectedDirContent, process_dir_listing(DirListing)).
+
+process_contex(ContexEntries) ->
+    ContextLines = lists:filter(fun(Line) ->
+                                        case Line of
+                                            ["$", "cd", _] -> true;
+                                            _ -> false
+                                        end
+                                end,
+                                ContexEntries),
+    {_, ReversedContexts} = lists:foldl(fun(Context, {_, []}) ->
+                                        [_, _, Dir] = Context,
+                                        {[Dir], [Dir]};
+                                   (Context, {Cwd, Contexts}) ->
+                                        [_, _, Dir] = Context,
+                                        case Dir of
+                                            ".." -> {tl(Cwd), Contexts};
+                                            _ -> {[Dir|Cwd], [[Dir|Cwd]|Contexts]}
+                                        end
+                                end,
+                                {"", []},
+                                ContextLines),
+    lists:reverse(lists:map(fun(Line) -> lists:reverse(Line) end, ReversedContexts)).
+
+process_contex_test() ->
+    Lines = aoc_input_app:read_file_lines("test_input_day07.txt", [split_lines_into_words]),
+    Contexts = process_contex(Lines),
+
+    ExpectedContexts = ["/", ["/", "a"], ["/", "a", "e"], ["/", "d"]],
+    ?assertEqual(ExpectedContexts, Contexts).
+
+process_entries_for_files(ContexEntries) ->
+    lists:foldl(fun(["$", "cd", "/"], {[], Acc}) ->
+                        {["/"], orddict:store(["/"], [], Acc)};
+                   (["$", "ls"], {Cwd, Acc}) ->
+                        {Cwd, Acc};
+                   (["$", "cd", ".."], {Cwd, Acc}) ->
+                        UpdatedCwd = lists:reverse(tl(lists:reverse(Cwd))),
+                        {UpdatedCwd, Acc};
+                   (["$", "cd", NewDir], {Cwd, Acc}) ->
+                        UpdatedCwd = lists:append(Cwd, [NewDir]),
+                        {UpdatedCwd, orddict:store(UpdatedCwd, [], Acc)};
+                   (["dir", _], {Cwd, Acc}) ->
+                        {Cwd, Acc};
+                   ([Size, File], {Cwd, Acc}) ->
+                        {CurrDirFiles, Dict} = orddict:take(Cwd, Acc),
+                        UpdatedDirFiles = lists:append(CurrDirFiles, [{Size, File}]),
+                        {Cwd, orddict:store(Cwd, UpdatedDirFiles, Dict)}
+                end,
+                {[], orddict:from_list([])},
+                ContexEntries).
+
+process_entries_for_files_test() ->
+    Lines = aoc_input_app:read_file_lines("test_input_day07.txt", [split_lines_into_words]),
+    ExpectedFiles = orddict:from_list(
+                     [
+                                {["/"],                                    [{"14848514", "b.txt"}, {"8504156", "c.dat"}]},
+                      {     ["/", "a"],                              [{"29116", "f"}, {"2557", "g"}, {"62596", "h.lst"}]},
+                      {["/", "a", "e"],                                                                   [{"584", "i"}]},
+                      {     ["/", "d"], [{"4060174", "j"}, {"8033020", "d.log"}, {"5626152", "d.ext"}, {"7214296", "k"}]}
+                     ]),
+    {_, Dirs} = process_entries_for_files(Lines),
+    ?assertEqual(ExpectedFiles, Dirs).
+
+process_entries_for_dirs(ContexEntries) ->
+    lists:foldl(fun(["$", "cd", "/"], {[], Acc}) ->
+                        {["/"], orddict:store(["/"], [], Acc)};
+                   (["$", "ls"], {Cwd, Acc}) ->
+                        {Cwd, Acc};
+                   (["$", "cd", ".."], {Cwd, Acc}) ->
+                        UpdatedCwd = lists:reverse(tl(lists:reverse(Cwd))),
+                        {UpdatedCwd, Acc};
+                   (["$", "cd", NewDir], {Cwd, Acc}) ->
+                        UpdatedCwd = lists:append(Cwd, [NewDir]),
+                        {UpdatedCwd, orddict:store(UpdatedCwd, [], Acc)};
+                   (["dir", Dir], {Cwd, Acc}) ->
+                        {CurrDirDirs, Dict} = orddict:take(Cwd, Acc),
+                        UpdatedDirDirs = lists:append(CurrDirDirs, [Dir]),
+                        {Cwd, orddict:store(Cwd, UpdatedDirDirs, Dict)};
+                   ([_, _], {Cwd, Acc}) ->
+                        {Cwd, Acc}
+                end,
+                {[], orddict:from_list([])},
+                ContexEntries).
+
+process_entries_for_dirs_test() ->
+    Lines = aoc_input_app:read_file_lines("test_input_day07.txt", [split_lines_into_words]),
+    ExpectedDirs = orddict:from_list(
+                     [
+                      {          ["/"], ["a", "d"]},
+                      {     ["/", "a"],      ["e"]},
+                      {["/", "a", "e"],         []},
+                      {     ["/", "d"],         []}
+                     ]),
+    {_, Dirs} = process_entries_for_dirs(Lines),
+    ?assertEqual(ExpectedDirs, Dirs).
+
+get_size_for_dir(Dir, DirsDict, FilesDict) ->
+    {CurDirFiles, _} = orddict:take(Dir, FilesDict),
+    SumSizeForFiles = lists:foldl(fun(Item, Acc) ->
+                                          {SizeStr, _} = Item,
+                                          {Size, _} = string:to_integer(SizeStr),
+                                          Acc + Size
+                                  end,
+                                  0,
+                                  CurDirFiles),
+
+    {CurDirDirs, _} = orddict:take(Dir, DirsDict),
+    SumSizeForDirs = lists:foldl(fun(Item, Acc) ->
+                                         DirToCheck = lists:append(Dir, [Item]),
+                                         Acc + get_size_for_dir(DirToCheck, DirsDict, FilesDict)
+                                 end,
+                                 0,
+                                 CurDirDirs),
+
+
+    SumSizeForDirs + SumSizeForFiles.
+
+get_size_for_dir_test() ->
+    Lines = aoc_input_app:read_file_lines("test_input_day07.txt", [split_lines_into_words]),
+    {_, DirsDict} = process_entries_for_dirs(Lines),
+    {_, FilesDict} = process_entries_for_files(Lines),
+
+    Dir1 = ["/", "d"],
+    ?assertEqual(4060174 + 8033020 + 5626152 + 7214296, get_size_for_dir(Dir1, DirsDict, FilesDict)),
+
+    Dir2 = ["/", "a", "e"],
+    ?assertEqual(584, get_size_for_dir(Dir2, DirsDict, FilesDict)),
+
+    Dir3 = ["/", "a"],
+    ?assertEqual(94853, get_size_for_dir(Dir3, DirsDict, FilesDict)),
+
+    Dir4 = ["/", "d"],
+    ?assertEqual(24933642, get_size_for_dir(Dir4, DirsDict, FilesDict)),
+
+    Dir5 = ["/"],
+    ?assertEqual(48381165, get_size_for_dir(Dir5, DirsDict, FilesDict)).
+
+part1(FileName) ->
+    Lines = aoc_input_app:read_file_lines(FileName, [split_lines_into_words]),
+    {_, DirsDict} = process_entries_for_dirs(Lines),
+    {_, FilesDict} = process_entries_for_files(Lines),
+
+    AllDirs = orddict:fetch_keys(DirsDict),
+    SizesList =lists:map(fun(Dir) ->
+                                 get_size_for_dir(Dir, DirsDict, FilesDict)
+                         end,
+                         AllDirs),
+    SizesAtMost100000 = lists:filter(fun(Size) -> Size =< 100000 end, SizesList),
+    lists:sum(SizesAtMost100000).
+
+part1_test() ->
+    TestResult = part1("test_input_day07.txt"),
+    ?debugFmt("Part1 test result: ~p\n", [TestResult]),
+    ?assertEqual(95437, TestResult),
+
+    Result = part1("input_day07.txt"),
+    ?debugFmt("Part1 result: ~p\n", [Result]),
+    ?assertEqual(1490523, Result).
+
+part2(FileName) ->
+    Lines = aoc_input_app:read_file_lines(FileName, [split_lines_into_words]),
+    {_, DirsDict} = process_entries_for_dirs(Lines),
+    {_, FilesDict} = process_entries_for_files(Lines),
+
+    TotalSpace = 70000000,
+    CurrOccupiedSpace = get_size_for_dir(["/"], DirsDict, FilesDict),
+    CurrFreeSpace = TotalSpace - CurrOccupiedSpace,
+
+    MinReqSpace = 30000000,
+    CurrReqSpace = MinReqSpace - CurrFreeSpace,
+
+    AllDirs = orddict:fetch_keys(DirsDict),
+    SizesList = lists:map(fun(Dir) ->
+                                  get_size_for_dir(Dir, DirsDict, FilesDict)
+                          end,
+                          AllDirs),
+    SizeEnoughList = lists:min(lists:sort(lists:filter(fun(Size) -> Size >= CurrReqSpace end, SizesList))).
+
+part2_test() ->
+    TestResult = part2("test_input_day07.txt"),
+    ?debugFmt("Part2 test result: ~p\n", [TestResult]),
+    ?assertEqual(24933642, TestResult),
+
+    Result = part2("input_day07.txt"),
+    ?debugFmt("Part2 result: ~p\n", [Result]),
+    %%?assertEqual(24933642, Result).
+    ok.
 
 -endif.
