d7b5f1b16a3a365d50d8be6f03e472eb3ec704a5
Michal Jurek <jurmcc@gmail.com>
2022-12-15 23:05:56 +0100

    Day10 work on progress

--- 2022/aoc_erlang/apps/day10/src/day10_app.erl
+++ 2022/aoc_erlang/apps/day10/src/day10_app.erl
@@ -16,7 +16,38 @@ part2(_FileName) ->
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
 
-first_test() ->
-    ?assertEqual(ok, ok).
+digest(Instructions) -> digest(Instructions, {1, []}).
+
+digest([], {Curr, Acc}) -> lists:reverse([Curr|Acc]);
+digest([H|T], {Curr, Acc}) ->
+    ?debugFmt("H: ~p, T: ~p, Curr: ~p, Acc: ~p~n", [H, T, Curr, Acc]),
+case H of
+        "noop" -> digest(T, {Curr, [Curr|Acc]});
+        {"addx", Val} ->  digest(T, {Curr+Val, [Curr,Curr|Acc]})
+    end.
+
+digest_test() ->
+    Instructions = ["noop",
+                    {"addx", 3},
+                    {"addx", -5}],
+    ?assertEqual([1, 1, 1, 4, 4, -1], digest(Instructions)).
+
+load_input(FileName) ->
+    Input = aoc_input_app:read_file_lines(FileName, [remove_line_breaks, split_lines_into_words]),
+    lists:map(fun(Line) ->
+                      case string:split(Line, " ") of
+                          ["noop"] -> "noop";
+                          ["addx", Val] -> {addx, element(1, string:to_integer(Val))}
+                      end
+              end,
+              Input).
+
+input_test() ->
+    Input = load_input("test_input_day10.txt"),
+    ?debugFmt("Input: ~p~n", [Input]),
+    Digested = digest(Input),
+    ?debugFmt("Digested: ~p~n", [Digested]),
+    ok.
+
 
 -endif.
