566c3824d532a71541e855506fda8885f4152e90
Michal Jurek <jurmcc@gmail.com>
2022-12-17 00:33:42 +0100

    Day10 part 1 solved

--- 2022/aoc_erlang/apps/day10/src/day10_app.erl
+++ 2022/aoc_erlang/apps/day10/src/day10_app.erl
@@ -4,9 +4,6 @@
 
 %%% Exported functions
 
-part1(_FileName) ->
-    ok.
-
 part2(_FileName) ->
     ok.
 
@@ -20,34 +17,46 @@ digest(Instructions) -> digest(Instructions, {1, []}).
 
 digest([], {Curr, Acc}) -> lists:reverse([Curr|Acc]);
 digest([H|T], {Curr, Acc}) ->
-    ?debugFmt("H: ~p, T: ~p, Curr: ~p, Acc: ~p~n", [H, T, Curr, Acc]),
-case H of
-        "noop" -> digest(T, {Curr, [Curr|Acc]});
-        {"addx", Val} ->  digest(T, {Curr+Val, [Curr,Curr|Acc]})
+    case H of
+        noop -> digest(T, {Curr, [Curr|Acc]});
+        {addx, Val} ->  digest(T, {Curr+Val, [Curr,Curr|Acc]})
     end.
 
 digest_test() ->
-    Instructions = ["noop",
-                    {"addx", 3},
-                    {"addx", -5}],
+    Instructions = [noop,
+                    {addx, 3},
+                    {addx, -5}],
     ?assertEqual([1, 1, 1, 4, 4, -1], digest(Instructions)).
 
 load_input(FileName) ->
     Input = aoc_input_app:read_file_lines(FileName, [remove_line_breaks, split_lines_into_words]),
     lists:map(fun(Line) ->
                       case string:split(Line, " ") of
-                          ["noop"] -> "noop";
-                          ["addx", Val] -> {addx, element(1, string:to_integer(Val))}
+                          [["noop"]] -> noop;
+                          [["addx", Val]] -> {addx, element(1, string:to_integer(Val))};
+                          What -> ?debugFmt("Got it: ~p~n", [What])
                       end
               end,
               Input).
 
-input_test() ->
+register_value_at_cycle_test() ->
     Input = load_input("test_input_day10.txt"),
-    ?debugFmt("Input: ~p~n", [Input]),
     Digested = digest(Input),
-    ?debugFmt("Digested: ~p~n", [Digested]),
-    ok.
+    %%?debugFmt("Digested: ~p~n", [Digested]),
+    ?assertEqual(21, lists:nth(20, Digested)),
+    ?assertEqual(19, lists:nth(60, Digested)),
+    ?assertEqual(18, lists:nth(100, Digested)),
+    ?assertEqual(21, lists:nth(140, Digested)),
+    ?assertEqual(16, lists:nth(180, Digested)),
+    ?assertEqual(18, lists:nth(220, Digested)).
 
+part1(FileName) ->
+    Digested = digest(InputInput = load_input(FileName)),
+    Cycles = [20, 60, 100, 140, 180, 220],
+    lists:sum([Cycle * lists:nth(Cycle, Digested) ||  Cycle <- Cycles]).
+
+part1_test() ->
+    ?assertEqual(13140, part1("test_input_day10.txt")),
+    ?assertEqual(13720, part1("input_day10.txt")).
 
 -endif.
