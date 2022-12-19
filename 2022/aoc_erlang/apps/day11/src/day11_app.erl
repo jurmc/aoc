3deae29de3ee0ac7c7823c16cf6f58fd7ae8d716
Jurek Michal-AMJ018 <Michal.Jurek@motorolasolutions.com>
2022-12-19 16:34:31 +0100

    Reading input for day11 done

--- 2022/aoc_erlang/apps/day11/src/day11_app.erl
+++ 2022/aoc_erlang/apps/day11/src/day11_app.erl
@@ -1,6 +1,6 @@
 -module(day11_app).
 
--record(monkey, {id, items = [], operation, test, recipient_for_true, recipient_for_false}).
+-record(monkey, {id, items = [], operator, operand, test, recipient_for_true, recipient_for_false}).
 
 -export([part1/1, part2/1]).
 
@@ -19,34 +19,53 @@ part2(_FileName) ->
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
 
-read_section([SectMonkeyId,SectItems,SectOperation,SectTest,SectRecipientTrue,SectRecipientFalse|T]) ->
-    ?debugFmt("SectRecipientFalse: ~p~n", [SectRecipientFalse]),
+read_monkey([SectMonkeyId,SectItems,SectOperation,SectTest,SectRecipientTrue,SectRecipientFalse|T]) ->
     ["Monkey", Id] = SectMonkeyId,
     ["Starting", "items:"|Items] = SectItems,
-    ["Operation:", "new", "=", "old", "*", Operation] = SectOperation,
+    ["Operation:", "new", "=", "old", Operator, Operand] = SectOperation,
     ["Test:", "divisible", "by", Test] = SectTest,
-    ["If", "true:", "throw", "to", "monkey", RecipentTrue] = SectRecipientTrue,
-    ["If", "false:", "throw", "to", "monkey", RecipentFales] = SectRecipientFalse,
+    ["If", "true:", "throw", "to", "monkey", RecipientTrue] = SectRecipientTrue,
+    ["If", "false:", "throw", "to", "monkey", RecipientFalse] = SectRecipientFalse,
 
-    #monkey{id                      = element(1, string:to_integer(Id)),
-            items                   = [element(1, string:to_integer(Item)) || Item <- SectItems],
-            operation               = Operation,
-            test                    = Test,
-            recipient_for_true      = SectRecipientTrue,
-            recipient_for_false     = SectRecipientFalse}.
+    Monkey = #monkey{id                      = element(1, string:to_integer(Id)),
+            items                   = [element(1, string:to_integer(Item)) || Item <- Items],
+            operator               = Operator,
+            operand                = element(1, string:to_integer(Operand)),
+            test                    = element(1, string:to_integer(Test)),
+            recipient_for_true      = element(1, string:to_integer(RecipientTrue)),
+            recipient_for_false     = element(1, string:to_integer(RecipientFalse))},
+    [Monkey, T]. 
 
-read_monkey(FileName) ->
+read_monkeys(FileName) -> 
     Input = aoc_input_app: read_file_lines(FileName, [remove_line_breaks, split_lines_into_words]),
-    read_section(Input).
+    read_monkeys(Input, []).
 
-monkey_read__test() ->
-    ?assertEqual(#monkey{
-                    id = 0,
-                    items = [79, 78],
-                    operation = 19,
-                    test = 23,
-                    recipient_for_true = 2,
-                    recipient_for_false = 3},
-                 read_monkey("test_input_day11.txt")).
+read_monkeys([], Acc) -> lists:reverse(Acc);
+read_monkeys(Input, Acc) ->
+    [Monkey, RestInput] = read_monkey(Input),
+    read_monkeys(RestInput, [Monkey|Acc]).
+
+monkey_read_test() ->
+    [FirstMonkey|RestMonkeys] = read_monkeys("test_input_day11.txt"),
+    ExpectedFirstMonkey = #monkey{
+                        id = 0,
+                        items = [79, 98],
+                        operator = "*",
+                        operand = 19,
+                        test = 23,
+                        recipient_for_true = 2,
+                        recipient_for_false = 3},
+    ?assertEqual(ExpectedFirstMonkey, FirstMonkey),
+
+    LastMonkey = lists:last(RestMonkeys),
+    ExpectedLastMonkey = #monkey{
+                            id = 3,
+                            items = [74],
+                            operator = "+",
+                            operand = 3,
+                            test = 17,
+                            recipient_for_true = 0,
+                            recipient_for_false = 1},
+    ?assertEqual(ExpectedLastMonkey, LastMonkey).
 
 -endif.
