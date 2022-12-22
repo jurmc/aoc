7eccaa7ef627fe63dbccd7f9c7f7f44e94a9bcd1
Michal Jurek <jurmcc@gmail.com>
2022-12-22 22:19:19 +0100

    Day 12 boilerplate

---
+++ 2022/aoc_erlang/apps/day12/src/day12_app.erl
@@ -0,0 +1,23 @@
+-module(day12_app).
+
+-export([part1/1, part2/1]).
+
+%%% Exported functions
+
+part1(_FileName) ->
+    ok.
+
+part2(_FileName) ->
+    ok.
+
+%%% Internal functions
+
+
+%%% Unit tests
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+
+first_test() ->
+    ?assertEqual(ok, ok).
+
+-endif.
