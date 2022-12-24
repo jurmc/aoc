4285d7efcab5576b0dac73de0a6bc33a83b90b38
Michal Jurek <jurmcc@gmail.com>
2022-12-24 01:02:44 +0100

    Day 12 part1 - maybe solved, definitely not optimal

--- 2022/aoc_erlang/apps/day12/src/day12_app.erl
+++ 2022/aoc_erlang/apps/day12/src/day12_app.erl
@@ -1,6 +1,11 @@
 -module(day12_app).
 
--export([part1/1, part2/1]).
+%%-export([part1/1, part2/1]).
+-compile(export_all).
+
+%%% Unit tests
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
 
 -define(UPPERCASE_S, 83).
 -define(UPPERCASE_E, 69).
@@ -8,11 +13,16 @@
 
 %%% Exported functions
 
-part1(_FileName) ->
-    ok.
-
 part2(_FileName) ->
     ok.
+
+part1(FileName) ->
+    {Begin, End, HeightMap} = load_input(FileName),
+    %%?debugFmt("Begin: ~p, End: ~p~n", [Begin, End]),
+    %%?debugFmt("HeightMap: ~n~p~n", [HeightMap]),
+    Paths = get_paths(Begin, End, HeightMap),
+    %%?debugFmt("Test paths: ~n~p~n", [Paths]),
+    lists:min([length(Path) || Path <- Paths]) - 1.
 
 %%% Internal functions
 
@@ -43,10 +53,6 @@ load_input(FileName) ->
     NormalizedCoordsList = normalize_height(CoordsList),
     {Begin, End, dict:from_list(NormalizedCoordsList)}.
 
-%%% Unit tests
--ifdef(TEST).
--include_lib("eunit/include/eunit.hrl").
-
 begin_end_coords_test() ->
     {Begin, End, _} = load_input("test_input_day12.txt"),
     ?assertEqual({1, 1}, Begin),
@@ -70,9 +76,17 @@ filter_directions({X, Y}, PotentialDirections, HeightMap, CurrPath) ->
                       end,
                       PotentialDirections),
     %% Do not go to a field that is already in our path
-    F2 = lists:filter(fun(Position) -> not lists:member(Position, CurrPath) end, F1).
-    %% TODO: apply rules imposed by the part1 logic (do not step onto filed that is too high
-
+    F2 = lists:filter(fun(NextPosition) -> not lists:member(NextPosition, CurrPath) end, F1),
+    %% Do not step onto field that is too high
+    {ok, CurrHeight} = dict:find({X, Y}, HeightMap),
+    F3 = lists:filter(fun(NextPosition) ->
+                              {ok, NextPositionHeight} = dict:find(NextPosition, HeightMap),
+                              %%?debugFmt("~nNextPosition: ~p~nCurrHeight: ~p, NextPositionHeight: ~p~n", [NextPosition, CurrHeight, NextPositionHeight]),
+                              CurrHeight + 1 >= NextPositionHeight
+                      end,
+                      F2),
+    %%?debugFmt("~nPotentialDirections: ~p~nF1: ~p~nF2: ~p~nF3: ~p~n", [PotentialDirections, F1, F2, F3]),
+    F3.
 
 filter_directions_test() ->
     {X,Y} = {1,1},
@@ -89,42 +103,46 @@ filter_directions_test() ->
     ?assertEqual(AllowedDirections, filter_directions({X, Y}, PotentialDirections, HeightMap, CurrPath)).
 
 get_paths(Begin, End, HeightMap) ->
-    get_paths(Begin, End, HeightMap, {[Begin], []}, 0).
+    {_, Paths} = get_paths(Begin, End, HeightMap, {[Begin], []}, 0),
+    Paths.
 
 get_paths(BeginEqualToEnd, BeginEqualToEnd, _HeightMap, _Acc = {CurrPath, OtherPaths}, Depth) ->
-    ?debugFmt("get_paths clause1~n", []),
+    %%?debugFmt("get_paths clause1~n", []),
     {[], [lists:reverse(CurrPath)|OtherPaths]};
 get_paths(Begin, End, _HeightMap, _Acc = {CurrPath, OtherPaths}, 2) ->
-    ?debugFmt("get_paths clause2~n", []),
+    %%?debugFmt("get_paths clause2~n", []),
     {[], [lists:reverse(CurrPath)|OtherPaths]};
 get_paths(Begin = {X1, Y1}, End = {X2, Y2}, HeightMap, Acc = {CurrPath, _OtherPaths}, Depth) ->
-    ?debugFmt("Begin: ~p, End: ~p, Acc: ~p~n", [Begin, End, Acc]),
+    %%?debugFmt("Begin: ~p, End: ~p, Acc: ~p~n", [Begin, End, Acc]),
     PotentialDirections = [{X1+1, Y1},
                            {X1-1, Y1},
                            {X1, Y1+1},
                            {X1, Y1-1}],
     FilteredDirections = filter_directions(Begin, PotentialDirections, HeightMap, CurrPath), %% TODO: I bet this part will have to be adapted somehow for part2 :)
     lists:foldl(fun(NewPositon, {_, OtherPaths}) ->
-                        ?debugFmt("NewPositon: ~p, End: ~p, CurrPath: ~p, OtherPaths: ~p, Depth: ~p", [NewPositon, End, CurrPath, OtherPaths, Depth]),
+                        %%?debugFmt("NewPositon: ~p, End: ~p, CurrPath: ~p, OtherPaths: ~p, Depth: ~p", [NewPositon, End, CurrPath, OtherPaths, Depth]),
                         %%Out = get_paths(NewPositon, End, HeightMap, {[NewPositon|CurrPath], OtherPaths}, Depth+1),
                         Out = get_paths(NewPositon, End, HeightMap, {[NewPositon|CurrPath], OtherPaths}, Depth),
-                        ?debugFmt("Out: ~p~n", [Out]),
+                        %%?debugFmt("Out: ~p~n", [Out]),
                         Out
                 end,
                 Acc,
                 FilteredDirections).
 
-get_paths_test() ->
-    Begin = {1,1},
-    End = {2,2},
-    HeightMap = dict:from_list([{{1,1}, 0},
-                             {{2,1}, 1},
-                             {{1,2}, 2},
-                             {{2,2}, 2}]),
-    Paths = get_paths(Begin, End, HeightMap),
-    ?debugFmt("Paths: ~n~p~n", [Paths]),
-    %%ExpectedPaths = dict:from_list([{1,1}, {2,1}, {2,2}]),
-    %%?assertEqual(ExpectedPaths, ).
-    ok.
+%%get_paths_test() ->
+%%    Begin = {1,1},
+%%    End = {2,2},
+%%    HeightMap = dict:from_list([{{1,1}, 0},
+%%                             {{2,1}, 1},
+%%                             {{1,2}, 2},
+%%                             {{2,2}, 2}]),
+%%    ExpectedPaths = [[{1,1}, {2,1}, {2,2}]],
+%%    Paths = get_paths(Begin, End, HeightMap),
+%%    ?debugFmt("Paths: ~n~p~n", [Paths]),
+%%    ?assertEqual(ExpectedPaths, Paths).
+
+part1_test() ->
+    Result = part1("test_input_day12.txt"),
+    ?debugFmt("Result: ~p~n", [length(List) || List <- Result]).
 
 -endif.
