0378e7d3c2100762623a016f590508ffc2948f45
Jurek Michal-AMJ018 <Michal.Jurek@motorolasolutions.com>
2023-12-08 13:05:45 +0100

    Day05 refactoring before part2 solution

--- 2023/aoc_rust/solutions/day05/src/main.rs
+++ 2023/aoc_rust/solutions/day05/src/main.rs
@@ -1,24 +1,61 @@
 use std::fs;
 use std::collections::HashMap;
+use std::str::Lines;
 
 fn main() {
-    //let input = fs::read_to_string("test_input_day05.txt").unwrap();
+    let input = fs::read_to_string("test_input_day05.txt").unwrap();
+    let lines = input.lines();
+    let (seeds, order, maps) = prepare_input(lines);
+    let test_result = solve1(&seeds, &order, &maps);
     let input = fs::read_to_string("input_day05.txt").unwrap();
+    let lines = input.lines();
+    let (seeds, order, maps) = prepare_input(lines);
+    let result = solve1(&seeds, &order, &maps);
 
-    let mut input_it = input.lines();
-    let seeds: Vec<_> = input_it.next()
+    println!("test result: {}", test_result);
+    println!("result: {}", result);
+}
+
+fn solve1(seeds: &Vec<u64>, order: &Vec<&str>, maps: &HashMap<&str, Vec<Vec<u64>>>) -> u64 {
+    let mut mapping_applied = seeds.clone();
+    order.iter().for_each(|label| {
+        let mapping_def = maps.get(label).unwrap();
+        let new_mappings_applied: Vec<_>  = mapping_applied.iter()
+            .map(|num| {
+                let mut new_val = *num;
+                let mut it = mapping_def.iter();
+                while let Some(map) = it.next() {
+                    let dst_range_start = map[0];
+                    let src_range_start = map[1];
+                    let range_len = map[2];
+                    if src_range_start <= *num && *num <= src_range_start + range_len {
+                        new_val = dst_range_start + (num - src_range_start);
+                        break;
+                    }
+                }
+                new_val
+        }).collect();
+        mapping_applied = new_mappings_applied.clone();
+    });
+
+    let result = mapping_applied.iter().min().unwrap();
+    *result
+}
+
+fn prepare_input(mut lines: Lines) -> (Vec<u64>, Vec<&str>, HashMap<&str, Vec<Vec<u64>>>) {
+    let seeds: Vec<_> = lines.next()
         .unwrap()
         .split(':').nth(1).unwrap()
         .split_whitespace()
         .map(|c_s| c_s.parse::<u64>().unwrap())
         .collect();
-    input_it.next();
-    println!("seeds: {:?}", seeds);
+    lines.next();
+    //println!("seeds: {:?}", seeds);
 
     let mut order: Vec<&str> = Vec::new();
     let mut maps: HashMap<&str, Vec<_>> = HashMap::new();
     let mut curr_label = "";
-    while let Some(line) = input_it.next() {
+    while let Some(line) = lines.next() {
         if line.contains("map") {
             let label = line.split_whitespace().nth(0).unwrap();
             let data_for_map_entry: Vec<Vec<u64>> = Vec::new();
@@ -34,36 +71,8 @@ fn main() {
             //let mut v: &mut Vec<Vec<u64>> = maps.get(&curr_label).unwrap();
             //v.push(values);
             maps.entry(curr_label).and_modify(|v| v.push(values));
-            println!("values: {:?}", line);
+            //println!("values: {:?}", line);
         }
     }
-
-    let mut mapping_applied = seeds.clone();
-    println!("seeds: {:?}", seeds);
-    order.iter().for_each(|label| {
-        println!("label: {}", label);
-        let mapping_def = maps.get(label).unwrap();
-        let new_mappings_applied: Vec<_>  = mapping_applied.iter()
-            .map(|num| {
-                let mut new_val = *num;
-                let mut it = mapping_def.iter();
-                while let Some(map) = it.next() {
-                    let dst_range_start = map[0];
-                    let src_range_start = map[1];
-                    let range_len = map[2];
-                    println!("dst_range_start {}, src_range_start {}, range_len {}", dst_range_start, src_range_start, range_len);
-                    if src_range_start <= *num && *num <= src_range_start + range_len {
-                        new_val = dst_range_start + (num - src_range_start);
-                        break;
-                    }
-                }
-                new_val
-        }).collect();
-        mapping_applied = new_mappings_applied.clone();
-        //dump_dict(&mapping_dict);
-        println!("new_mappings_applied: {:?}", new_mappings_applied);
-    });
-
-    println!("result: {}", mapping_applied.iter().min().unwrap());
+    (seeds, order, maps)
 }
-
