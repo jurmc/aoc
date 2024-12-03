4674250e13a472133d361b3f6074d37d7ae416e0
Michal Jurek <jurmcc@gmail.com>
2024-12-03 23:42:31 +0100

    Day02 ongoing

--- 2024/day02/src/main.rs
+++ 2024/day02/src/main.rs
@@ -1,44 +1,40 @@
 use std::fs;
 use std::collections::HashSet;
 
-fn is_row_safe(row: &Vec<i32>) -> bool {
-    let diff_pattern = HashSet::from([1, 2, 3]);
-    let diffs: HashSet<i32> = row.windows(2).map(|i| {i32::abs(i[0]-i[1])}).collect();
-    diffs.is_subset(&diff_pattern)
-}
-
 fn main() {
     let input = fs::read_to_string("input.txt").unwrap();
+    //let input = fs::read_to_string("test_input.txt").unwrap();
 
     let rows: Vec<Vec<i32>> = input.lines().map(|line| {
         let row: Vec<i32> = line.split_whitespace().map(|i| i.parse::<i32>().unwrap()).collect();
         row
     }).collect();
 
-    let (only_sorted_rows, not_sorted): (Vec<_>, Vec<_>) = rows.into_iter().partition(|row| { row.is_sorted() || row.iter().rev().is_sorted()});
 
-    let (proper_diff, mut not_safe) : (Vec<Vec<i32>>, Vec<Vec<i32>>) = only_sorted_rows.into_iter().partition(
-        |row| {
-            is_row_safe(&row)
-        });
-
-    let mut drops_at_first = not_sorted;
-    drops_at_first.append(&mut not_safe);
-
-    let proper_diff2: Vec<Vec<i32>> = drops_at_first.into_iter().filter(|row| {
-        for idx in 0..row.len() {
-            let mut new_row = row.clone();
-            new_row.remove(idx);
-            //print!(" new_row: {:?}", new_row);
-            if is_row_safe(&new_row) {
-                //println!(" safe");
-                return true
-            }
-            //println!(" not safe");
+    let (safe_rows, unsafe_rows): (Vec<_>, Vec<_>) = rows.iter().partition(|row| {
+        let mut direction = row[1] - row[0];
+        if direction > 0 {
+            direction = 1;
+        } else if direction < 0 {
+            direction = -1;
         }
-        false
-    }).collect();
+        let is_row_safe = row.windows(2).all(|win| {
+            let mut curr_diff = win[1] - win[0];
+            let curr_abs_diff = i32::abs(curr_diff);
 
-    println!("Result1: {}", proper_diff.len());
-    println!("Result2: {}", proper_diff.len() + proper_diff2.len());
+            if curr_diff * direction < 0 {
+                return false;
+            }
+
+            if curr_abs_diff > 3 || curr_abs_diff < 1 {
+                return false;
+            }
+
+            true
+        });
+        is_row_safe
+    });
+
+    println!("Result1: {}", &safe_rows.len());
+    println!("Unsafe rows: {}", &unsafe_rows.len());
 }
